529acf6969ff9a8c95c4f894d097644cab6ba09b
I-H <teemu@rautasydan.org>
2018-07-07 01:54:46 +0300

    Initial commit: Point-to-click movement works, Camera Zones work.

---
+++ Assets/Scripts/CameraZone.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class CameraZone : MonoBehaviour {
+    public Transform CameraPosition;
+    public Transform CameraFocusPoint;
+    Collider _c;
+
+    private void Awake()
+    {
+        _c = GetComponent<Collider>();
+        _c.isTrigger = true;
+    }
+    private void Start()
+    {
+        if(CameraFocusPoint == null)
+        {
+            CameraFocusPoint = new GameObject("Camera Focus Point").transform;
+            CameraFocusPoint.position = transform.position;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player") return;
+
+        Camera.main.transform.position = CameraPosition.position;
+        Camera.main.transform.LookAt(CameraFocusPoint);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_c == null) _c = GetComponent<Collider>();
+        Gizmos.DrawWireCube(_c.bounds.center, _c.bounds.size);
+    }
+}
---
+++ Assets/Scripts/PointClickMovement.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PointClickMovement : MonoBehaviour {
+    public string WalkableTag = "Walkable";
+    public float MoveSpeed = 0.5f;
+    private float _moveThreshold = 0.1f;
+    private Vector3 _moveTarget = new Vector3();
+    private Rigidbody _rb;
+    private Vector3 _moveDir = new Vector3();
+    private bool _atTarget = false;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        CheckInput();
+        MoveTowardsTarget();
+    }
+
+    private void MoveTowardsTarget()
+    {
+        Vector3 distance = _moveTarget - transform.position;
+        _moveDir = distance;
+        _moveDir.y = 0;
+        _moveDir = _moveDir.normalized;
+        if(_atTarget = distance.magnitude > _moveThreshold)
+        {
+            //_rb.AddForce(_moveDir * MoveSpeed);
+            //_rb.velocity = _moveDir*MoveSpeed;
+            _moveTarget.y = transform.position.y;
+            _rb.MovePosition(Vector3.MoveTowards(transform.position, _moveTarget, MoveSpeed/10.0f));
+        }
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray mouseWorld = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit mouseHit;
+            if (Physics.Raycast(mouseWorld, out mouseHit) && mouseHit.collider.tag == WalkableTag)
+            {
+                _moveTarget = mouseHit.point;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = _atTarget ? Color.yellow : Color.green;
+        Gizmos.DrawSphere(_moveTarget, 1.0f);
+        Gizmos.DrawRay(transform.position, _moveDir * 2);
+    }
+}
